2da1f92c5937e508765abcab2cd63e8cc9b32c49
Siddhart <Siddhart_7@live.nl>
2023-06-15 00:33:14 +0200

    added Google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const client = new ApolloClient({
   uri: 'https://us-east-1-shared-usea1-02.cdn.hygraph.com/content/clhvznxbz10sb01tbempl16ux/master',
@@ -8,9 +9,14 @@ const client = new ApolloClient({
 });
 
 function MyApp({ Component, pageProps }) {
-  return <ApolloProvider client={client}>
-    <Component {...pageProps} />
-  </ApolloProvider>
+  return (
+    <>
+      <GoogleAnalytics gaMeasurementId='G-G774GS5VKK' trackPageViews />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  )
 }
 
 export default MyApp
